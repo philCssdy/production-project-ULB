438c0276c54395cb79e945e749b03ac9de07b594
PhShk <Mazfakertrim@yandex.ru>
2026-02-18 16:44:01 +0300

    add styleLint, add jest, add first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,36 @@
+import { classNames } from "./classNames";
+
+describe('classNames', () => {
+    test ('with only first param', () => {   
+        expect(classNames('someClass')).toBe('someClass');
+    
+    })
+    test ('with aditional classes', () => {  
+        const expepcted = "someClass class1 class2"
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expepcted)
+    
+    })
+    test ('with mods', () => {  
+        const expepcted = "someClass class1 class2 hovered scrollable"
+        expect(classNames('someClass', 
+            {hovered: true, scrollable:true}, 
+            ['class1', 'class2'])).toBe(expepcted)
+    
+    })
+
+    test ('with mods false' , () => {  
+        const expepcted = "someClass class1 class2 hovered"
+        expect(classNames('someClass', 
+            {hovered: true, scrollable:false}, 
+            ['class1', 'class2'])).toBe(expepcted)
+    
+    })
+    test ('with mods undefined' , () => {  
+        const expepcted = "someClass class1 class2 hovered"
+        expect(classNames('someClass', 
+            {hovered: true, scrollable:undefined}, 
+            ['class1', 'class2'])).toBe(expepcted)
+    
+    })
+    
+})
---
+++ stylelint.config.cjs
@@ -0,0 +1,8 @@
+// @ts-nocheck
+/** @type {import('stylelint').Config} */
+module.exports = {
+    extends: ["stylelint-config-standard-scss"],
+    rules: {
+        "selector-class-pattern": null
+    }
+}; 
